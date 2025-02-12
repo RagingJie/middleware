48821fa376adebe503b5fce553d41e977291d380
Naruto <2045933252@qq.com>
2025-02-12 17:46:54 +0800

    中间件--【redis】--配置RedissonClient

---
+++ redis/src/main/java/com/study/redis/config/RedissonConfig.java
@@ -0,0 +1,23 @@
+package com.study.redis.config;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * description: RedissionConfig
+ */
+@Configuration
+public class RedissonConfig {
+
+    @Bean
+    public RedissonClient RedissonClient() {
+        // 配置
+        Config config = new Config();
+        config.useSingleServer().setAddress("redis://127.0.0.1:6379").setPassword("123456");
+        // 创建RedissonClient对象
+        return Redisson.create(config);
+    }
+}
