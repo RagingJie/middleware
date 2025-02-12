819819c131352cc37d583070f53193025a3c8be9
Naruto <2045933252@qq.com>
2025-02-12 17:47:12 +0800

    中间件--【redis】--释放锁，使用lua脚本保证key的原子性，防止key被其他线程修改

--- redis/src/main/java/com/study/redis/utils/SimpleRedisLock.java
+++ redis/src/main/java/com/study/redis/utils/SimpleRedisLock.java
@@ -1,8 +1,12 @@
 package com.study.redis.utils;
 
 import cn.hutool.core.lang.UUID;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
+import org.springframework.data.redis.core.script.RedisScript;
 
+import java.util.Collections;
 import java.util.concurrent.TimeUnit;
 
 public class SimpleRedisLock implements ILock {
@@ -11,12 +15,25 @@ public class SimpleRedisLock implements ILock {
     private static final String KEY_PREFIX = "lock:";
     private static final String ID_PREFIX = UUID.randomUUID().toString(true) + "-";
     private StringRedisTemplate stringRedisTemplate;
+    private static final DefaultRedisScript<Long> UNLOCK_SCRIPT;
+
+    static {
+        UNLOCK_SCRIPT = new DefaultRedisScript<>();
+        UNLOCK_SCRIPT.setLocation(new ClassPathResource("unlock.lua"));
+        UNLOCK_SCRIPT.setResultType(Long.class);
+    }
 
     public SimpleRedisLock(String name, StringRedisTemplate stringRedisTemplate) {
         this.name = name;
         this.stringRedisTemplate = stringRedisTemplate;
     }
 
+    /**
+     * 尝试获取锁
+     *
+     * @param timeoutSec 锁持有的超时时间，过期后会自动释放，单位秒
+     * @return
+     */
     @Override
     public boolean tryLock(long timeoutSec) {
         // 获取当前线程的id
@@ -27,7 +44,17 @@ public class SimpleRedisLock implements ILock {
         return Boolean.TRUE.equals(success);
     }
 
+    /**
+     * 释放锁，使用lua脚本保证key的原子性，防止key被其他线程修改
+     */
     @Override
+    public void unlock() {
+        stringRedisTemplate.execute(UNLOCK_SCRIPT,
+                Collections.singletonList(KEY_PREFIX + name),
+                ID_PREFIX + Thread.currentThread().getId());
+    }
+
+ /*   @Override
     public void unlock() {
         // 获取当前线程的id
         String threadId = ID_PREFIX + Thread.currentThread().getId();
@@ -38,5 +65,5 @@ public class SimpleRedisLock implements ILock {
             // 释放锁
             stringRedisTemplate.delete(KEY_PREFIX + name);
         }
-    }
+    }*/
 }
